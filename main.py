27c539b5d18c83e6e70c32eb1303e134206b6a67
Navneet Panchayan <navneet.panchayan@bitdurg.ac.in>
2023-12-19 22:00:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ def run_python_verbose():
     try:
         # Run the 'python -v' command in a subprocess
         print("okk")
-        subprocess.run(['python', '-v'], check=True)
+        subprocess.run(['python', 'ytdlbot/ytdl_bot.py'], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running 'python -v': {e}")
 
 # Call the function
 
 app = FastAPI()
-run_python_verbose()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+@app.get("/okk")
+async def okk():
+    run_python_verbose()
+    
