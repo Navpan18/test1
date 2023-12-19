0588328902702aa6adf4521f724988b34a537f36
Navneet Panchayan <navneet.panchayan@bitdurg.ac.in>
2023-12-19 22:36:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 import subprocess
-
+import requests
 def run_python_verbose():
     try:
         # Run the 'python -v' command in a subprocess
@@ -18,5 +18,23 @@ async def root():
     return {"message": "Hello World"}
 @app.get("/okk")
 async def okk():
-    run_python_verbose()
+    url = 'https://ytdlbot2.onrender.com/'
+    timeout_seconds = 5
+    
+    try:
+        # Make a GET request with a timeout
+        response = requests.get(url, timeout=timeout_seconds)
+        response.raise_for_status()  # Raise an exception for bad responses (4xx and 5xx status codes)
+        
+        # Process the response here
+        print("Request successful:", response.text)
+        return {"message": "okk"}
+    
+    except requests.exceptions.Timeout:
+        print(f"Request to {url} timed out after {timeout_seconds} seconds.")
+        return {"message": "timed out"}
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Request to {url} failed: {e}")
+        return {"message": "kuch aur"}
     
